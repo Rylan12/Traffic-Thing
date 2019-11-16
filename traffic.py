e4b5c799dc4c7eb73a0b76405d73d357e9388181
Rylan Polster <rslpolster@gmail.com>
2019-11-16 12:40:25 -0500

    Add traffic classes

---
+++ traffic.py
@@ -0,0 +1,31 @@
+# Inputs: car sensed at x traffic light, data of all the roads (length, width, location, speed limit),
+#         intersections (each with 4 lights),
+
+# Outputs: When and what traffic light to turn off
+
+
+class Light:
+
+    def __init__(self, direction, state, ID):
+        self.direction = direction
+        self.state = state
+        self.ID = ID
+
+    def changeState(st8):
+        state = st8
+
+
+class Intersection:
+
+    def __init__(self, x, y, numLights, ID):
+        self.x = x
+        self.y = y
+        self.numLights = numLights
+        self.ID = ID
+
+
+class Street:
+
+    def __init__(self, speedLimit, inter1, inter2):
+        self.speedLimit = speedLimit
+        self.length = (inter2.x - inter1.x) + (inter2.y - inter1.x)  # distance between
